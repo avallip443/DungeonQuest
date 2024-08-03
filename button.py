b8df481ebd06b93e3647591f58fc8b13b52cea3f
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-08-02 23:19:03 -0400

    refactor button

--- button.py
+++ button.py
@@ -1,14 +1,29 @@
 import pygame
 
+
 class Button:
-    def __init__(self, surface, x, y, image, width, height):
+    def __init__(
+        self,
+        surface: pygame.Surface,
+        x: int,
+        y: int,
+        image: pygame.Surface,
+        width: int,
+        height: int,
+    ):
         self.image = pygame.transform.scale(image, (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect = self.image.get_rect(center=(x, y))
         self.clicked = False
         self.surface = surface
 
-    def draw(self):
+    def draw(self) -> bool:
+        """
+        Draw the button on the surface and handle click events.
+
+        Returns:
+            bool: True if the button was clicked, False otherwise.
+        """
+        
         action = False
         pos = pygame.mouse.get_pos()
 
@@ -17,10 +32,11 @@ class Button:
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 action = True
                 self.clicked = True
+            else:
+                self.clicked = False
 
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
         self.surface.blit(self.image, (self.rect.x, self.rect.y))
         return action
-    
