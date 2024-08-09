edc51d6688c6fc44f92eaf6713fcf6a958e3178c
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-08-09 02:04:07 -0400

    create fighter superclass

--- enemy.py
+++ enemy.py
@@ -1,21 +1,8 @@
 import pygame
-from random import randint, random
-import math
-from animations import load_character_animations
-from enum import Enum
-from battle import display_damage
+from fighter import Fighter, Action
 
 
-class Action(Enum):
-    IDLE = "idle"
-    ATTACK = "attack"
-    SPECIAL = "special"
-    HURT = "hurt"
-    DEATH = "death"
-    WALK = "walk"
-
-
-class Enemy:
+class Enemy(Fighter):
     def __init__(
         self,
         name: str,
@@ -23,70 +10,8 @@ class Enemy:
         strength: int,
         crit_chance: int,
     ):
-        self.name: str = name
-        self.max_hp: int = max_hp
-        self.hp: int = max_hp
-        self.strength: int = strength
-        self.crit_chance: int = crit_chance
-
-        self.alive: bool = True
-        self.action: Action = Action.IDLE
-        self.delayed_damage: int = 0
-
-        self.x_pos: int = 800
-        self.y_pos: int = 0
+        super().__init__(name, max_hp, strength, crit_chance, x_pos=800, y_pos=0)
         self.hitbox = pygame.Rect(0, 0, 100, 120)
-
-        self.animation_timer: int = 0
-        self.delay_counter: int = 0
-        self.current_frame_index: int = 0
-        self.animations = load_character_animations()
-
-        self.death_animation_done: bool = False
-        self.death_animation_timer: int = 0
-
-    def take_damage(self, damage: int) -> None:
-        """
-        Apply delayed damage to the enemy.
-
-        Args:
-            damage (int): Amount of damage to be taken.
-        """
-
-        self.delayed_damage = damage
-        self.active = self.hp > 0
-        self.delay_counter = 10
-
-    def attack(self) -> int:
-        """
-        Calculate the damage dealt by the enemy.
-
-        Returns:
-            int: Damage value including potential critical hit.
-        """
-
-        damage = self.strength + randint(-5, 5)
-        self.action = Action.ATTACK
-        self.animation_timer = 0
-
-        if random() < self.crit_chance / 100:
-            damage *= 1.5
-
-        return math.floor(damage)
-
-    def apply_damage(self) -> None:
-        """
-        Apply damage and update the action based on remaining HP
-        """
-
-        self.hp = max(self.hp - self.delayed_damage, 0)
-        self.alive = self.hp > 0
-
-        self.action = Action.HURT if self.alive else Action.DEATH
-        self.animation_timer = 0
-
-        display_damage(self, self.delayed_damage, (255, 0, 0))
-        self.delayed_damage = 0
 
     def walk(self, target_x: int) -> None:
         """
@@ -124,48 +49,6 @@ class Enemy:
         """
 
         self.hitbox.topleft = (x_pos, y_pos)
-
-    def update_animation(self) -> None:
-        """
-        Update the enemy's animation based on the current action and timers.
-        """
-
-        if self.delay_counter > 0:
-            self.delay_counter -= 1
-            if self.delay_counter == 0:
-                self.apply_damage()
-            return
-
-        current_animation = self.animations[self.name.lower()][self.action.value]
-        animation_length = current_animation.get_frame_count()
-
-        if self.action == Action.DEATH:
-            self.update_death_animation(animation_length)
-        else:
-            self.update_regular_animation(animation_length)
-
-    def update_death_animation(self, animation_length: int) -> None:
-        """
-        Update death animation to ensure it runs only once.
-        """
-
-        if not self.death_animation_done:
-            self.death_animation_timer += 1
-            if self.death_animation_timer >= animation_length:
-                self.death_animation_done = True
-
-    def update_regular_animation(self, animation_length: int) -> None:
-        """
-        Update animations for actions other than death.
-        """
-
-        if self.action != Action.IDLE:
-            self.animation_timer += 1
-            if self.animation_timer >= animation_length:
-                self.animation_timer = 0
-                self.current_frame_index = 0
-                if self.action != Action.WALK:
-                    self.action = Action.IDLE
 
 
 def create_enemy(index: int) -> Enemy:
--- player.py
+++ player.py
@@ -1,20 +1,8 @@
 from random import randint, random
-import math
-from animations import load_character_animations
-from enum import Enum
-from battle import display_damage
+from fighter import Fighter, Action
 
 
-class Action(Enum):
-    IDLE = "idle"
-    ATTACK = "attack"
-    SPECIAL = "special"
-    HURT = "hurt"
-    DEATH = "death"
-    WALK = "walk"
-
-
-class Player:
+class Player(Fighter):
     def __init__(
         self,
         name: str,
@@ -25,40 +13,10 @@ class Player:
         potion_chance: int,
         potions: int,
     ):
-        self.name: str = name
-        self.max_hp: int = max_hp
-        self.hp: int = max_hp
-        self.strength: int = strength
-        self.crit_chance: int = crit_chance
+        super().__init__(name, max_hp, strength, crit_chance, x_pos=0, y_pos=0)
         self.double_chance: int = double_chance
         self.potion_chance: int = potion_chance
         self.potions: int = potions
-
-        self.alive: bool = True
-        self.action: Action = Action.IDLE
-        self.delayed_damage = 0
-
-        self.x_pos: int = 0
-        self.y_pos: int = 0
-        
-        self.animation_timer: int = 0
-        self.delay_counter: int = 0
-        self.current_frame_index: int = 0
-        self.animations = load_character_animations()
-        
-        self.death_animation_done: bool = False
-        self.death_animation_timer: int = 0
-        
-    def take_damage(self, damage: int) -> None:
-        """
-        Apply delayed damage to the enemy.
-
-        Args:
-            damage (int): Amount of damage to be taken.
-        """
-        self.delayed_damage = damage
-        self.active = self.hp > 0
-        self.delay_counter = 10
 
     def heal(self) -> int:
         """
@@ -67,7 +25,7 @@ class Player:
         Returns:
             int: Heal amount to player's health.
         """
-        
+
         heal_amount = 30 + randint(-5, 5)
         self.hp = min(self.hp + heal_amount, self.max_hp)
         self.potions -= 1
@@ -75,38 +33,18 @@ class Player:
 
     def attack(self) -> int:
         """
-        Calculate the damage dealt by the enemy.
+        Calculate the damage dealt by the player.
 
         Returns:
             int: Damage value including potential critical hit.
         """
-        damage = self.strength + randint(-5, 5)
-        self.action = Action.ATTACK
-        self.animation_timer = 0
-
-        if random() < self.crit_chance / 100:
-            damage += 1.5
-            self.action = Action.SPECIAL
+        damage = super().attack()
 
         if random() < self.double_chance / 100:
             damage *= 2
             self.action = Action.SPECIAL
 
-        return math.floor(damage)
-
-    def apply_damage(self) -> None:
-        """
-        Apply damage and update the action based on remaining HP
-        """
-
-        self.hp = max(self.hp - self.delayed_damage, 0)
-        self.alive = self.hp > 0
-
-        self.action = Action.HURT if self.alive else Action.DEATH
-        self.animation_timer = 0
-
-        display_damage(self, self.delayed_damage, (255, 0, 0))
-        self.delayed_damage = 0
+        return damage
 
     def walk(self, target_x: int) -> None:
         """
@@ -134,49 +72,6 @@ class Player:
                 if self.x_pos >= target_x:
                     self.action = Action.IDLE
 
-    def update_animation(self) -> None:
-        """
-        Updates the player's animation and processes delayed actions like applying damage.
-        """
-        if self.delay_counter > 0:
-            self.delay_counter -= 1
-            if self.delay_counter == 0:
-                self.apply_damage()
-            return
-
-        current_animation = self.animations[self.name.lower()][self.action.value]
-        animation_length = current_animation.get_frame_count()
-
-        if self.action == Action.DEATH:
-            self.update_death_animation(animation_length)
-        else:
-            self.update_regular_animation(animation_length)
-                    
-    def update_death_animation(self, animation_length: int) -> None:
-        """
-        Update death animation to ensure it runs only once.
-        """
-
-        if not self.death_animation_done:
-            self.death_animation_timer += 1
-            if self.death_animation_timer >= animation_length:
-                self.death_animation_done = True
-
-    def update_regular_animation(self, animation_length: int) -> None:
-        """
-        Update animations for actions other than death.
-        """
-
-        if self.action != Action.IDLE:
-            self.animation_timer += 1
-            if self.animation_timer >= animation_length:
-                self.animation_timer = 0
-                self.current_frame_index = (
-                    self.current_frame_index + 1
-                ) % animation_length
-                if self.action != Action.WALK:
-                    self.action = Action.IDLE
-
 
 def create_character(index: int) -> Player:
     characters = [
@@ -198,7 +93,7 @@ def animate_player(screen, player: Player, animations, scale: float) -> None:
     scale = scale - 0.5 if change_scale else scale
 
     current_animation = animations[player.name.lower()][player.action.value]
-    
+
     if player.death_animation_done:
         current_frame = current_animation.get_last_death_frame(scale=scale)
     else:
