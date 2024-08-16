3fefa837f3c0dbcc5bd068852d52ffe2b036043f
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-08-16 17:04:10 -0400

    refactr button

--- button.py
+++ button.py
@@ -11,6 +11,17 @@ class Button:
         width: int,
         height: int,
     ):
+        """
+        Initialize a Button object.
+
+        Args:
+            surface (pygame.Surface): Surface on which the button will be drawn.
+            x (int): X-coordinate for the button's center position.
+            y (int): Y-coordinate for the button's center position.
+            image (pygame.Surface): Image representing the button.
+            width (int): Width of the button.
+            height (int): Height of the button.
+        """
         self.image = pygame.transform.scale(image, (width, height))
         self.rect = self.image.get_rect(center=(x, y))
         self.clicked = False
@@ -26,16 +37,12 @@ class Button:
         action = False
         pos = pygame.mouse.get_pos()
 
-        # check mouseover and clicked conditions
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 action = True
                 self.clicked = True
-            else:
+            elif pygame.mouse.get_pressed()[0] == 0:
                 self.clicked = False
-
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
 
         self.surface.blit(self.image, (self.rect.x, self.rect.y))
         return action
